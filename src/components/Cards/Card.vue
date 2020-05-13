07bbe617d014da39f0037c0e168349f0a79e98c5
Daniel Mamgren de Oliveira <danielmamgrendeoliveira@Daniels-MacBook-Pro.local>
2020-05-13 21:21:40 +0200

    adding grind around card

--- src/components/Cards/Card.vue
+++ src/components/Cards/Card.vue
@@ -1,102 +1,140 @@
-<template>
-
-  <!-- <v-card
+  <!-- <v-container>
+    <v-card
     class="mx-auto"
     max-width="344"
-    outlined
-  >
-    <v-list-item three-line>
-      <v-list-item-content>
-        <div class="overline mb-4">Infected</div>
-        <v-list-item-title class="headline mb-1">REAL DATA</v-list-item-title>
-        <v-list-item-title class="headline mb-1">REAL DATA</v-list-item-title>
-        <v-list-item-subtitle>Number of active cases of COVID-19</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-  </v-card> -->
-
-<!-- TEST 2 -->
-    <v-container>
-        <v-card
-        class="mx-auto"
-        max-width="344"
     >
-        <v-row dense>
-            <!-- <v-col
-            v-for="card in cards"
-            :key="card.title"
-            :cols="card.flex"
-            > -->
-
-            <div>
-            <!-- INFECTED -->
-
-                <v-card>
-                    <v-list-item three-line>
-                        <v-list-item-content>
-                            <div class="overline mb-4">Infected</div>
-                            <v-list-item-title class="headline mb-1">{{data.confirmed.value}}</v-list-item-title>
-                            <v-list-item-title class="headline mb-1">REAL DATA</v-list-item-title>
-                            <v-list-item-subtitle>Number of active cases of COVID-19</v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>
-            </v-card>
-
-            </div>
-            
-            <div>
-            <!-- RECOVERED -->
-                <v-card>
-                    <v-list-item three-line>
-                        <v-list-item-content>
-                            <div class="overline mb-4">Recovered</div>
-                            <v-list-item-title class="headline mb-1">{{data.recovered.value}}</v-list-item-title>
-                            <v-list-item-title class="headline mb-1">Last Updated: {{ data.lastUpdate | date }}</v-list-item-title>
-                            <v-list-item-subtitle>Number of recoveries from COVID-19</v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>
-            </v-card>
-        </div>
-
-        <div>
-          <!-- DEATHS -->
-            <v-card>
-                <v-list-item three-line>
-                    <v-list-item-content>
-                        <div class="overline mb-4">Deaths</div>
-                        <v-list-item-title class="headline mb-1">{{data.deaths.value}}</v-list-item-title>
-                        <v-list-item-title class="headline mb-1"> <a :href="data.deaths.detail" target="_blank">Further details</a></v-list-item-title>
-                        <v-list-item-subtitle>Number of deaths caused by COVID-19</v-list-item-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
+      <v-row dense>
+        <div class="infected">
+          <v-card>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="overline mb-4">Infected</div>
+                <v-list-item-title class="headline mb-1">{{data.confirmed.value}}</v-list-item-title>
+                <v-list-item-title class="headline mb-1">REAL DATA</v-list-item-title>
+                <v-list-item-subtitle>Number of active cases of COVID-19</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
           </v-card>
         </div>
+          
+        <div class="recovered">
+            <v-card>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <div class="overline mb-4">Recovered</div>
+                  <v-list-item-title class="headline mb-1">{{data.recovered.value}}</v-list-item-title>
+                  <v-list-item-title class="headline mb-1">Last Updated: {{ data.lastUpdate | date }}</v-list-item-title>
+                  <v-list-item-subtitle>Number of recoveries from COVID-19</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-card>
+        </div>
 
-        <!-- </v-col> -->
+        <div class="deaths">
+          <v-card>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="overline mb-4">Deaths</div>
+                <v-list-item-title class="headline mb-1">{{data.deaths.value}}</v-list-item-title>
+                <v-list-item-title class="headline mb-1"> <a :href="data.deaths.detail" target="_blank">Further details</a></v-list-item-title>
+                <v-list-item-subtitle>Number of deaths caused by COVID-19</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </div>
       </v-row>
-  </v-card>
-</v-container>
+    </v-card>
+  </v-container> -->
 
+ <template>
+  <v-container v-if="!data" class="grey lighten-5 mx-2">Loading...</v-container>
+  <v-container v-else class="grey lighten-5 mx-2">
+    <v-row class="ma-auto">
+      <v-col cols="8">
+        <v-card 
+          class="pa-2"
+          width="350px"
+        >
+           <v-list-item>
+              <v-list-item-content>
+                <div class="overline mb-4">Infected</div>
+                <v-list-item-title class="headline mb-1">{{data.confirmed.value}}</v-list-item-title>
+                <v-list-item-title class="headline mb-1"> <a :href="data.deaths.detail" target="_blank">Further details</a></v-list-item-title>
+                <v-list-item-subtitle>Number of deaths caused by COVID-19</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="8">
+        <v-card 
+          class="pa-2"
+          width="350px"
+        >
+           <v-list-item three-line>
+              <v-list-item-content>
+                <div class="overline mb-4">Recovered</div>
+                <v-list-item-title class="headline mb-1">
+                  <CountUp 
+                    start = {0}
+                    end = {data.recovered.value}
+                    duration = {2.5
+                    separator=','                  
+                  />
+                  {{data.recovered.value}}</v-list-item-title>
+                <v-list-item-title class="headline mb-1"> <a :href="data.deaths.detail" target="_blank">Further details</a></v-list-item-title>
+                <v-list-item-subtitle>Number of deaths caused by COVID-19</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="8">
+        <v-card 
+          class="pa-2"
+          width="350px"
+        >
+           <v-list-item three-line>
+              <v-list-item-content>
+                <div class="overline mb-4">Deaths</div>
+                <v-list-item-title class="headline mb-1">
+                  <ICountUp 
+                    :delay = "delay"
+                    :endVal = "endVal"
+                    :options = "options"                  
+                  />
+                  
+                  </v-list-item-title>
+                <v-list-item-title class="headline mb-1"> <a :href="data.deaths.detail" target="_blank">Further details</a></v-list-item-title>
+                <v-list-item-subtitle>Number of deaths caused by COVID-19</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  export default {
-    props: ['data'],
-    data: () => ({
-      cards: [
-        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 12 },
-        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
-        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
-      ],
-    }),
+import ICountUp from 'vue-countup-v2';
+import CountUp from 'countup.js'
+export default {
+  props: ['data'],
+  data() {
+    return {
+       delay: 100,
+        endVal: 500000,
+        options: {
+          useEasing: true,
+          useGrouping: true,
+          separator: ',',
+    }
+    }
+  },
+  components: {
+    ICountUp
   }
-</script>
-
-<style scoped>
-
-a {
-    text-decoration: none;
-    cursor: pointer;
 }
-
-</style>
+</script>
